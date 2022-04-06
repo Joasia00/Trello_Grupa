c675cfe241cfcbb6ec6b16580ad0f991142c84ff
Kamil Lipiński <kamil.lipinski192@gmail.com>
2022-04-06 19:49:43 +0200

    Utworzylem obiekty klasy Osoba

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -5,5 +5,10 @@ public class Main {
         // Zadanie 1 wykonane
         // Nie rozumiem polecenia zad 3 ale ok
 
+        Osoba osoba1 = new Osoba("Marian", "Kowalski");
+        Osoba osoba2 = new Osoba("Adrian", "Roztocki");
+        Osoba osoba3 = new Osoba("Hugo", "Kołątaj");
+        Osoba osoba4 = new Osoba("Alicja", "Wkrainieczarów");
+        Osoba osoba5 = new Osoba("Dagmara", "Drakula");
     }
 }
