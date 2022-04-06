cb00cf909eeaed284667a6df3aea2216103d96b8
Joasia00 <Asiawegiel123@wp.pl>
2022-04-06 10:48:02 +0200

    Drugi commit

--- src/main/java/Main/Bear.java
+++ src/main/java/Main/Bear.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Bear {
+public class Bear {
+    public static void main(String[] args) {
+        System.out.println("Marian");
+        int liczba = 32;
+        System.out.println(liczba);
+        System.out.println("Olek");
+        int liczba2 = 2;
+        System.out.println(liczba2);
+        System.out.println("Filipek");
+        int liczba3 = 10;
+        System.out.println(liczba3);
+        System.out.println("Teddy");
+        int liczba4 = 3;
+        System.out.println(liczba4);
+
+    }
 }
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -3,5 +3,7 @@ package Main;
 public class Main {
     public static void main(String[] args) {
         // Zadanie 1 wykonane
+        // Nie rozumiem polecenia zad 3 ale ok
+
     }
 }
